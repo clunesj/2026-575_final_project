be4a18926d77352e330db1b60893dbe224149c0a
Joseph Kowalczyk <jmkowalczyk@wisc.edu>
2026-04-19 14:26:57 -0500

    Installed Leaflet.js, added map and temporary basemap

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
-//insert code here!
+// main.js, by Joseph Kowalczyk, James Clunes, and Brooke Fandrich
+(function(){
+    // Pseudoglobal Variables
+    var map;
+
+    // Creating leaflet map
+    function mapInit() {
+        map = L.map('map').setView([43.08, -89.38], 13);
+
+        // Add tileset
+        L.tileLayer('https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.{ext}', {
+	        minZoom: 0,
+	        maxZoom: 20,
+	        attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	        ext: 'png'
+        }).addTo(map);
+
+    }
+    // Runs mapInit() once the DOM loads.
+    document.addEventListener('DOMContentLoaded', mapInit)
+})(); // Must be the last line. Closes and executes the wrapping function. ---------------------------------------
