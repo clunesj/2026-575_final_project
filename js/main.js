341b87cc546ee0be90f330956f5b71ef8f03699a
Joseph Kowalczyk <jmkowalczyk@wisc.edu>
2026-04-23 13:08:23 -0500

    Basic framework for time and frequency filters + Added the basic HTML containers for time and frequency filter controls. - Currently non-functional, awaiting completed base dataset

--- js/main.js
+++ js/main.js
@@ -5,7 +5,9 @@
 
     // Creating leaflet map
     function mapInit() {
-        map = L.map('map').setView([43.08, -89.38], 13);
+        map = L.map('map', {
+            zoomControl: false // Remove zoom control to reposition later.
+        }).setView([43.08, -89.38], 13);
 
         // Add tileset
         L.tileLayer('https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.{ext}', {
@@ -22,14 +24,89 @@
     // AddData function, will eventually load in prebuilt dataset and user's chosen locations, currently handles menus
     function addData(map) {
         createNavMenu();
+        // Search goes here
+        createTimeFilter();
+        createRepeatFilter();
+        createZoom();
     };
 
     // Create search control NOTE: Requires leaflet-search.js to be installed, or other similar library!
 
     // Create time filter control
+    function createTimeFilter() {
+        // Initialize filter menu
+        var timeFilter = L.Control.extend({
+            options: {position: 'topleft'},
+
+            onAdd: function () {
+                // Create control container div
+                var container = L.DomUtil.create('div', 'timefilter-container');
+
+                // Container content
+                var content = L.DomUtil.create('div', 'timefilter-content', container);
+                content.innerHTML = `
+                    <p>Location Availability</p>
+                    <div>
+                        <input type = 'radio' id = 'rightnow' name = 'availability' value = 'rightnow'>
+                        <label for = 'rightnow'>Right Now</label>
+                    </div>
+                    <div>
+                        <input type = 'radio' id = 'anytime' name = 'availability' value = 'anytime'>
+                        <label for = 'anytime'>Any Time</label>
+                    </div>
+                    <div>
+                        <input type = 'radio' id = 'custom' name = 'availability' value = 'rightnow'>
+                        <label for = 'custom'>Custom</label>
+                    </div>
+                `;
+
+                // Disable click propogation
+                L.DomEvent.disableClickPropagation(container);
+
+                return container;
+            }
+        });
+
+        map.addControl(new timeFilter())
+    };
 
     // Create event repeat filter control
+        function createRepeatFilter() {
+        // Initialize filter menu
+        var repeatFilter = L.Control.extend({
+            options: {position: 'topleft'},
 
+            onAdd: function () {
+                // Create control container div
+                var container = L.DomUtil.create('div', 'repeatfilter-container');
+
+                // Container content
+                var content = L.DomUtil.create('div', 'repeatfilter-content', container);
+                content.innerHTML = `
+                    <p>Location Frequency</p>
+                    <div>
+                        <input type = 'radio' id = 'all' name = 'frequency' value = 'all'>
+                        <label for = 'all'>All</label>
+                    </div>
+                    <div>
+                        <input type = 'radio' id = 'recurring' name = 'frequency' value = 'recurring'>
+                        <label for = 'recurring'>Recurring</label>
+                    </div>
+                    <div>
+                        <input type = 'radio' id = 'popup' name = 'frequency' value = 'popup'>
+                        <label for = 'popup'>Pop-up (one-time)</label>
+                    </div>
+                `;
+
+                // Disable click propogation
+                L.DomEvent.disableClickPropagation(container);
+
+                return container;
+            }
+        });
+
+        map.addControl(new repeatFilter())
+    };
     // Create navigation menu (hosting dashboard, personal dashboard, settings, log in)
     function createNavMenu() {
         var navMenu = L.Control.extend({
@@ -46,11 +123,11 @@
                 // Create content/expanded appearance
                 var content = L.DomUtil.create('div', 'navmenu-content', container);
                 content.innerHTML = `
-                        <p><a href=''>Hosting Dashboard</a></p>
-                        <p><a href=''>Personal Dashboard</a></p>
-                        <p><a href=''>Settings</a></p>
-                        <p><a href=''>Log In</a></p>
-                    `;
+                    <p><a href=''>Hosting Dashboard</a></p>
+                    <p><a href=''>Personal Dashboard</a></p>
+                    <p><a href=''>Settings</a></p>
+                    <p><a href=''>Log In</a></p>
+                `;
 
                 // Toggling the content on/off when the menu button is clicked
                 L.DomEvent.on(button, 'click', function(e){ // Leaflet's version of addEventListener for the button
@@ -70,6 +147,13 @@
 
         map.addControl(new navMenu());
     };
+
+    // Readd zoom control in the bottom right, making room for filter controls
+    function createZoom() {
+        L.control.zoom({position: 'bottomleft'}).addTo(map)
+    };
+
     // Runs mapInit() once the DOM loads.
     document.addEventListener('DOMContentLoaded', mapInit)
+
 })(); // Must be the last line. Closes and executes the wrapping function. ---------------------------------------
