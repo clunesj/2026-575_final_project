4c910766dd99e6c4912395a5690ae41a2bb750d7
jamesclunes <jamesclunes27@gmail.com>
2026-05-04 11:08:06 -0500

    quick map update!
    changed basemap from osm_bright to stamen_terrain

--- js/main.js
+++ js/main.js
@@ -29,7 +29,7 @@
         }).setView([43.08, -89.38], 13);
 
         // Add tileset
-        L.tileLayer('https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.{ext}', {
+        L.tileLayer('https://tiles.stadiamaps.com/tiles/stamen_terrain/{z}/{x}/{y}{r}.{ext}', {
             minZoom: 0,
             maxZoom: 20,
             attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
